c5820adf693a8fe1cbf1de28c0d664c84b8aec45
Simon Armstrong <nitrologic@gmail.com>
2015-11-11 12:52:22 +1300

    gyro readblock

--- sensehat/i2c.cpp
+++ sensehat/i2c.cpp
@@ -41,8 +41,22 @@ int main(){
 	}
 
 	if (ioctl(file, I2C_SLAVE, 0x6a) < 0) {
-		printf("Error: Could not select magnetometer\n");
+		printf("Error: Could not select gyro\n");
 	}
+// Enable Gyro
+	writeGyrReg(CTRL_REG1_G, 0b00001111); // Normal power mode, all axes enabled
+	writeGyrReg(CTRL_REG4_G, 0b00110000); // Continuos update, 2000 dps full scale	
+	
+	while(true){
+        uint8_t b[6];
+        readBlock(0x80 | OUT_X_L_A, sizeof(b), b); 		
+		printf("packet $d $d $d  $d $d $d\n,b[0],b[1],b[2],b[3],b[4],b[5]);
+	}
+	
+// Enable accelerometer.	
+//	writeAccReg(CTRL_REG1_XM, 0b01100111); //  z,y,x axis enabled, continuos update,  100Hz data rate
+//	writeAccReg(CTRL_REG2_XM, 0b00100000); // +/- 16G full scale	
+	
 	close(file);
 
 	return 0;
